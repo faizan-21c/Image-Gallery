c17ddc7b58437a5dc4d4f9dc66a3c37ac5371ee5
faizan-21c <faizanmansuri78722@gmail.com>
2025-08-24 10:58:38 +0530

    CodeAlpha

---
+++ script.js
@@ -0,0 +1,11 @@
+function showCategory(category) {
+    let images = document.querySelectorAll('#gallery img');
+    
+    images.forEach(img => {
+        if (category === 'all' || img.classList.contains(category)) {
+            img.style.display = 'block';
+        } else {
+            img.style.display = 'none';
+        }
+    });
+}
